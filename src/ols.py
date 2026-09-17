01a80e306d459024b4ba1da704a6eea1acc6a5fb
KJnomy <krishnajainvapk@gmail.com>
2026-09-18 01:28:46 +0530

    Update ols.py

--- src/ols.py
+++ src/ols.py
@@ -1,8 +1,9 @@
 import statsmodels.tsa.stattools as sm
+import statsmodels.api as sma
 
 
 def fit_hedge_ratio(train_y, train_x):
-    train_x = sm.add_constant(train_x)
+    train_x = sma.add_constant(train_x)
 
     model = sm.OLS(train_y, train_x)
     result = model.fit()
